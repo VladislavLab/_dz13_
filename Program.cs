4e23f786d4bf2b5d386df38b3a3280925d3cb906
Vladislav <chupakhin.vladislav@gmail.com>
2023-02-06 11:11:55 +0200

    показ втор цифру

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 13: Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает,что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число  ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num>99)
+{
+  while (num>999)
+  {
+    num/=10;
+    
+  }
+  int i= num%10;
+  Console.Write($" {i} ");
+}
+else{
+  Console.Write("нет 3й цифры ");
+}
